4193ac85eae0e2cba96b92f0d7673547fce78646
Sgissa <salmanegarba721@gmail.com>
2023-11-17 14:59:44 -0500

    first version of pokemon generator

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import React from 'react';
+import 'semantic-ui-css/semantic.css';
+import '@/styles/globals.css';
+import { Menu, Input, Button } from 'semantic-ui-react';
+import Link from 'next/link';
+import styles from '@/styles/App.module.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [pokemonSearch, setPokemonSearch] = React.useState('');
+
+  function updateSearch(e, { value }) {
+    setPokemonSearch(value);
+  }
+
+  return (
+  <>
+  <Menu>
+  <Menu.Item className={styles.homeNav} as={Link} href='/'>Home</Menu.Item>
+  <Menu.Item className={styles.bar}>
+    <Input name= 'pokemonSearch' onChange={updateSearch} value={pokemonSearch}/>
+    <Button as={Link} href={`/pokemon/${pokemonSearch}`}>Search</Button>
+  </Menu.Item>
+  </Menu>
+  <Component {...pageProps} />
+  
+  </>
+  
+  );
+
 }
---
+++ src/pages/pokemon/[name].js
@@ -0,0 +1,70 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Loader, Image, Button, Icon, Label } from 'semantic-ui-react';
+import styles from '@/styles/name.module.css';
+
+
+export default function PokemonName(){
+    const [pokemonInfo, setPokemonInfo] = React.useState({ loading: true })
+    const router = useRouter();
+    const ButtonExampleLabeledBasic = () => (
+        <div>
+          <Button as='div' labelPosition='right'>
+            <Button color='red'>
+              <Icon name='heart' />
+              Like
+            </Button>
+            <Label as='a' basic color='red' pointing='left'>
+              2,048
+            </Label>
+          </Button>
+          <Button as='div' labelPosition='right'>
+            <Button basic color='blue'>
+              <Icon name='Dislike' />
+              Dislike
+            </Button>
+            <Label as='a' basic color='blue' pointing='left'>
+              2,048
+            </Label>
+          </Button>
+        </div>
+      )
+
+    React.useEffect(function(){
+        if(pokemonInfo.name !== router.query.name && router.query.name) {
+            console.log('Load in Pokemon info', router.query.name)
+            fetch(`https://pokeapi.co/api/v2/pokemon/${router.query.name}`)
+            .then(r=> r.json())
+            .then(function (r) {
+                setPokemonInfo(r);
+            })
+            .catch((e) =>setPokemonInfo({ loading: false, name: router.query.name }));
+        }
+    })
+
+    //console.log(router.query);
+    return (
+        <>
+            <h1>Pokemon Name: {router.query.name}</h1>
+            <Loader active={pokemonInfo.loading || pokemonInfo.name !== router.query.name}/>
+            {
+                // if this statment
+                pokemonInfo.id 
+                //then do this; ture
+                ? (  <>
+                    <Image className={styles.pic} src={pokemonInfo.sprites.front_default}></Image>
+                    <ButtonExampleLabeledBasic/>
+                    <h2>Shiny: {router.query.name}</h2>
+                    <Image className={styles.pic} src={pokemonInfo.sprites.front_shiny}></Image>
+                </>
+                //else do this; false
+               ) : ( 
+               <>
+                {isNaN(pokemonInfo.id) ? <h2>Searching for Pokemon</h2> : <h2>Pokemon Not Found</h2>}
+                </>
+                )
+            }
+            <ButtonExampleLabeledBasic/>
+        </>
+    )
+}
